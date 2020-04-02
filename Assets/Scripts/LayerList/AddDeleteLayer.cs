e5d080dd5317ec858495351dc85d08ff7471291d
ETdoFresh <ETdoFresh@gmail.com>
2020-04-01 23:21:32 -0500

    Add ability to delete layer.

---
+++ Assets/Scripts/LayerList/AddDeleteLayer.cs
@@ -0,0 +1,19 @@
+﻿// TODO: Only allow remove layers if > 1 exists.
+
+public class AddDeleteLayer : ECSSystem
+{
+    private void Update()
+    {
+        foreach (var entity in GetEntities<MouseDownEvent, RemoveActiveLayerButton>())
+        {
+            var activeLayer = GetEntityItem1<ActiveLayer>();
+            if (activeLayer && activeLayer.active)
+            {
+                var grid = activeLayer.active.GetComponent<GridData>();
+                foreach(var cell in grid.cells)
+                    cell.Destroy();
+                Destroy(activeLayer.active.gameObject);
+            }
+        }
+    }
+}
