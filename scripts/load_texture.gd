04eb001f8b7a3edc10009393d3cb36279f8b33b9
ETdoFresh <ETdoFresh@gmail.com>
2020-07-11 18:33:41 -0500

    Add demo for load_texture and xml_parser

--- scripts/load_texture.gd
+++ scripts/load_texture.gd
@@ -5,6 +5,15 @@ func _ready():
     $Main/VBoxContainer/DownloadPanel/Button.connect("pressed", self, "download_image")
     $HTTPRequest.connect("request_completed", self, "download_complete")
 
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/GridContainer/Columns.connect("focus_entered", self, "show_highlight_columns")
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/GridContainer/Columns.connect("focus_exited", self, "hide_highlight_columns")
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/GridContainer/Rows.connect("focus_entered", self, "show_highlight_rows")
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/GridContainer/Rows.connect("focus_exited", self, "hide_highlight_rows")
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/GridContainer/Spacing.connect("focus_entered", self, "show_highlight_spacing")
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/GridContainer/Spacing.connect("focus_exited", self, "hide_highlight_spacing")
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/GridContainer/Padding.connect("focus_entered", self, "show_highlight_padding")
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/GridContainer/Padding.connect("focus_exited", self, "hide_highlight_padding")
+
 func download_image():
     $HTTPRequest.request($Main/VBoxContainer/DownloadPanel/TextEdit.text)
 
@@ -20,3 +29,27 @@ func download_complete(_result, _response_code, _headers, body):
     $Main/VBoxContainer/ImagePanel/TextureRect.texture = texture
     $Main/VBoxContainer/DownloadPanel.visible = false
     $Main/VBoxContainer/ImagePanel.visible = true
+
+func show_highlight_columns():
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/Panel/columns.visible = true
+
+func hide_highlight_columns():
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/Panel/columns.visible = false
+
+func show_highlight_rows():
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/Panel/rows.visible = true
+
+func hide_highlight_rows():
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/Panel/rows.visible = false
+
+func show_highlight_spacing():
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/Panel/spacing.visible = true
+
+func hide_highlight_spacing():
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/Panel/spacing.visible = false
+
+func show_highlight_padding():
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/Panel/padding.visible = true
+
+func hide_highlight_padding():
+    $Main/VBoxContainer/SettingsPanel/HBoxContainer/Panel/padding.visible = false
--- scripts/main.gd
+++ scripts/main.gd
@@ -7,6 +7,8 @@ func _ready():
     $VBoxContainer/Button2.connect("pressed", self, "load_wfc")
     $VBoxContainer/Button3.connect("pressed", self, "load_simplex")
     $VBoxContainer/Button4.connect("pressed", self, "load_random")
+    $VBoxContainer/Button5.connect("pressed", self, "load_load_texture")
+    $VBoxContainer/Button6.connect("pressed", self, "load_xml_atlas")
 
 func load_paint_grid():
     get_tree().change_scene("res://scenes/paint_grid.tscn")
@@ -19,3 +21,9 @@ func load_simplex():
 
 func load_random():
     get_tree().change_scene("res://scenes/random_noise_demo.tscn")
+
+func load_load_texture():
+    get_tree().change_scene("res://scenes/load_texture.tscn")
+
+func load_xml_atlas():
+    get_tree().change_scene("res://scenes/load_texture_atlas_xml.tscn")
