bec8572d4a838c8332ef7ac84021aa4df136c312
ETdoFresh <ETdoFresh@gmail.com>
2020-07-30 14:19:55 -0500

    Add initial concept of changing layer types.

--- scenes/map_editor/layer.gd
+++ scenes/map_editor/layer.gd
@@ -10,9 +10,12 @@ export var normal_color = Color.transparent
 export var selected_color = Color.blue
 
 func _ready():
-    $HBoxContainer/Label.text = "%s (%s)" % [name, layer_type]
     var _1 = $HBoxContainer/Label/ButtonBehavior.connect("pressed", self, "emit_pressed_signal")
     var _2 = $HBoxContainer/CheckBox.connect("toggled", self, "emit_visibility_toggled_signal")
+    update_text()
+
+func update_text():
+    $HBoxContainer/Label.text = "%s (%s)" % [name, layer_type]
 
 func emit_pressed_signal():
     emit_signal("pressed")
--- scenes/map_editor/layers_editor.gd
+++ scenes/map_editor/layers_editor.gd
@@ -1,11 +1,14 @@
 extends Control
 
+var layer_types = ["Paint", "Random", "Simplex", "Wave Func. Col."]
+
 onready var layers = $VBoxContainer/ScrollContainer/Layers
 onready var move_layer_up = $VBoxContainer/GridContainer/MoveLayerUp
 onready var move_layer_down = $VBoxContainer/GridContainer/MoveLayerDown
 onready var add_layer = $VBoxContainer/GridContainer/AddLayer
 onready var delete_layer = $VBoxContainer/GridContainer/DeleteLayer
 onready var rename_layer = $VBoxContainer/GridContainer/RenameLayer
+onready var option_button = $VBoxContainer/HBoxContainer/OptionButton
 
 func _ready():
     layers.connect("selection_changed", self, "update_buttons")
@@ -14,6 +17,17 @@ func _ready():
     move_layer_up.connect("pressed", layers, "move_selected_up")
     move_layer_down.connect("pressed", layers, "move_selected_down")
     update_buttons()
+    add_layer_types_to_dropdown()
+    option_button.connect("item_selected", self, "print_selected")
+
+func print_selected(selected):
+    if layers.selected != null:
+        layers.selected.layer_type = option_button.text
+        layers.selected.update_text()
+
+func add_layer_types_to_dropdown():
+    for layer_type in layer_types:
+        option_button.add_item(layer_type)
 
 func update_buttons():
     if layers.selected == null:
