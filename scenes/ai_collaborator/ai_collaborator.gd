a515031cb402bd039e67905b853ffe27d2a53d96
ETdoFresh <ETdoFresh@gmail.com>
2020-10-04 16:19:54 -0500

    Start creating ai_collaborator

---
+++ scenes/ai_collaborator/ai_collaborator.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var tileset: Tileset
+var rules: Rules
+var user_map
+var generated_map
+
+func set_tileset(set_tileset):
+    tileset = set_tileset
+
+func set_rules(set_rules):
+    rules = set_rules
+
+func set_user_map(set_user_map):
+    user_map = set_user_map
+
+func run_wfc():
+    pass
+
+func display_generated_map():
+    pass
